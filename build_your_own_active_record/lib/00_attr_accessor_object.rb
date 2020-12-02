214fa338db1d099ba916e583bd94a1203199d624
Jose Miralles <josemiralles1991@gmail.com>
2020-12-02 15:57:19 -0500

    Phase o

--- build_your_own_active_record/lib/00_attr_accessor_object.rb
+++ build_your_own_active_record/lib/00_attr_accessor_object.rb
@@ -1,5 +1,8 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |arg|
+      self.class_eval("def #{arg};@#{arg};end")
+      self.class_eval("def #{arg}=(val);@#{arg}=val;end")
+    end
   end
 end
